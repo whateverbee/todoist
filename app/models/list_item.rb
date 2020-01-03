4001dc2e4947d59d56f33e156c50449c20ff7918
Alida Weatherbee <alida.weatherbee@shopify.com>
2020-01-03 14:39:10 -0500

    rest of CRUD + complete fn

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -1,3 +1,7 @@
 class ListItem < ApplicationRecord
   belongs_to :list
+
+  def completed?
+    !completed_at.blank?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :lists do
-    resources :list_items
+    resources :list_items do
+      member do
+        patch :complete
+      end
+    end
   end
   root 'lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
