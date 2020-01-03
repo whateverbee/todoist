35b55c20bb9d75221b2ea3d3263b1be8bfc8607c
Alida Weatherbee <alida.weatherbee@shopify.com>
2020-01-03 13:43:11 -0500

    adding list items

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,3 @@
 class List < ApplicationRecord
+  has_many :todo_items
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_03_165757) do
+ActiveRecord::Schema.define(version: 2020_01_03_170318) do
+
+  create_table "list_items", force: :cascade do |t|
+    t.string "content"
+    t.integer "list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["list_id"], name: "index_list_items_on_list_id"
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
@@ -19,4 +27,5 @@ ActiveRecord::Schema.define(version: 2020_01_03_165757) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "list_items", "lists"
 end
